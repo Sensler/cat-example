4927a31ef79489ede8f590efda411db1d39bda0c
MaPiGo <markus_piatkowski@gothaer-systems.de>
2018-10-24 11:42:30 +0200

    app-cat added

---
+++ app-cat/src/main/java/de/cat/rest/appcat/AppCatApplication.java
@@ -0,0 +1,12 @@
+package de.cat.rest.appcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCatApplication.class, args);
+	}
+}
