2ec303d789f1369879cd468818cff6e8228c9b87
MaPiGo <markus_piatkowski@gothaer-systems.de>
2018-10-24 13:12:12 +0200

    hello controller implementation

---
+++ app-cat/src/test/java/de/cat/rest/appcat/HelloControllerUnitTest.java
@@ -0,0 +1,22 @@
+package de.cat.rest.appcat;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class HelloControllerUnitTest {
+
+    @Test
+    public void hello() {
+        HelloController controller = new HelloController();
+        assertThat(controller.hello(), is("Hello World"));
+    }
+
+    @Test
+    public void helloName() {
+        HelloController controller = new HelloController();
+        String cat = "Cat";
+        assertThat(controller.hello(cat), is("Hello " + cat));
+    }
+}
